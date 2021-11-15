dd49634cf63464e9e0bf263017b8031e1c65ea0c
Ivan Krakhmaliuk <ivan_krakhmaliuk@epam.com>
2021-11-15 13:20:18 +0200

    Modules: WIP changes

--- pylon/core/tools/module.py
+++ pylon/core/tools/module.py
@@ -21,9 +21,11 @@ import io
 import os
 import sys
 import json
+import types
 import shutil
 import zipfile
 import tempfile
+import posixpath
 import importlib
 import pkg_resources
 
@@ -54,6 +56,12 @@ class ModuleDescriptior:
         self.settings = settings
         self.root_path = root_path
 
+    def make_blueprint(self):
+        """ Make configured Blueprint instance """
+
+    def template_name(self, name, module=None):
+        """ Make prefixed template name """
+
 
 class ModuleManager:
     """ Manages modules """
@@ -61,28 +69,26 @@ class ModuleManager:
     def __init__(self, context):
         self.context = context
         self.settings = self.context.settings
-        self.modules = dict()  # module_name -> (module_root_path, module_metadata, module_obj)
-        self.metadata = dict()  # module_obj -> module_metadata
         #
+        self.modules = dict()  # module_name -> module_descriptor
         self.temporary_data_dirs = list()
-        # Register provider for template and resource loading from modules
-        pkg_resources.register_loader_type(DataModuleLoader, DataModuleProvider)
-
-    def add_module(self, module_name, module_root_path, module_metadata, module_obj):
-        """ Register module """
-        self.modules[module_name] = (module_root_path, module_metadata, module_obj)
-        self.metadata[module_obj] = (module_root_path, module_metadata, module_name)
-
-    def get_module(self, module_name):
-        """ Get loaded module """
-        return self.modules.get(module_name, None)
-
-    def get_metadata(self, module_obj):
-        """ Get module metadata """
-        return self.metadata.get(module_obj, None)
 
     def init_modules(self):
         """ Load and init modules """
+        if self.context.debug and os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+            log.info(
+                "Running in development mode before reloader is started. Skipping module loading"
+            )
+            return
+        # Disable bytecode caching and register resource providers
+        sys.dont_write_bytecode = True
+        pkg_resources.register_loader_type(DataModuleLoader, DataModuleProvider)
+        # Make plugins holder
+        if "plugins" not in sys.modules:
+            sys.modules["plugins"] = types.ModuleType("plugins")
+            sys.modules["plugins"].__path__ = []
+        #
+        #
         if not self.context.debug:
             self.temporary_data_dirs = load_modules(self.context)
         else:
@@ -115,7 +121,7 @@ def load_modules(context):
             continue
         try:
             # Make loader for this module
-            module_loader = module.DataModuleLoader(module_data)
+            module_loader = DataModuleLoader(module_data)
             # Load module metadata
             if "metadata.json" not in module_loader.storage_files:
                 log.error("No module metadata, skipping")
@@ -267,17 +273,71 @@ def load_development_modules(context):
     return temporary_data_dirs
 
 
-class DataModuleLoader(importlib.abc.MetaPathFinder):
-    """ Allows to load modules from ZIP in-memory data """
 
-    def __init__(self, module_data):
-        self.storage = zipfile.ZipFile(io.BytesIO(module_data))
-        self.storage_files = [item.filename for item in self.storage.filelist]
+
+class LocalModuleLoader(importlib.machinery.PathFinder):
+    """ Allows to load modules from specific location """
+
+    def __init__(self, module_name, module_path):
+        self.module_name = module_name
+        self.module_name_components = self.module_name.split(".")
+        self.module_path = module_path
+        self.module_abspath = os.path.abspath(self.module_path)
 
     def _fullname_to_filename(self, fullname):
         base = fullname.replace(".", os.sep)
         # Try module directory
-        filename = os.path.join(base, "__init__.py")
+        filename = os.path.join(self.module_abspath, base, "__init__.py")
+        if os.path.isfile(filename):
+            return filename
+        # Try module file
+        filename = os.path.join(self.module_abspath, f"{base}.py")
+        if os.path.isfile(filename):
+            return filename
+        # Not found
+        return None
+
+    def find_spec(self, fullname, path=None, target=None):
+        """ Find spec for new module """
+        name_components = fullname.split(".")
+        if name_components[:len(self.module_name_components)] != self.module_name_components:
+            return None
+        #
+        filename = self._fullname_to_filename(
+            ".".join(name_components[len(self.module_name_components):])
+        )
+        if filename is None:
+            return None
+        #
+        return importlib.util.spec_from_file_location(fullname, filename)
+
+    def get_data(self, path):
+        """ Read data resource """
+        try:
+            with open(os.path.join(self.module_abspath, path), "rb") as file:
+                data = file.read()
+            return data
+        except BaseException as exc:
+            raise FileNotFoundError(f"Resource not found: {path}") from exc
+
+    def has_directory(self, path):
+        """ Check if directory is present in module """
+        return os.path.isdir(os.path.join(self.module_abspath, path))
+
+
+class DataModuleLoader(importlib.abc.MetaPathFinder):
+    """ Allows to load modules from ZIP in-memory data """
+
+    def __init__(self, module_name, module_data):
+        self.module_name = module_name
+        self.module_name_components = self.module_name.split(".")
+        self.storage = zipfile.ZipFile(io.BytesIO(module_data))
+        self.storage_files = [item.filename for item in self.storage.filelist]
+
+    def _fullname_to_filename(self, fullname):
+        base = fullname.replace(".", posixpath.sep)
+        # Try module directory
+        filename = posixpath.join(base, "__init__.py")
         if filename in self.storage_files:
             return filename, True
         # Try module file
@@ -287,11 +347,18 @@ class DataModuleLoader(importlib.abc.MetaPathFinder):
         # Not found
         return None, None
 
-    def find_spec(self, fullname, path, target=None):  # pylint: disable=W0613
+    def find_spec(self, fullname, path=None, target=None):  # pylint: disable=W0613
         """ Find spec for new module """
-        filename, is_package = self._fullname_to_filename(fullname)
+        name_components = fullname.split(".")
+        if name_components[:len(self.module_name_components)] != self.module_name_components:
+            return None
+        #
+        filename, is_package = self._fullname_to_filename(
+            ".".join(name_components[len(self.module_name_components):])
+        )
         if filename is None:
             return None
+        #
         return importlib.machinery.ModuleSpec(
             fullname, self, origin=filename, is_package=is_package
         )
@@ -303,26 +370,145 @@ class DataModuleLoader(importlib.abc.MetaPathFinder):
     def exec_module(self, module):
         """ Execute new module """
         module.__file__ = module.__spec__.origin
-        module.__dict__["__file__"] = module.__file__
+        module.__cached__ = None
+        #
         with self.storage.open(module.__file__, "r") as file:
-            exec(file.read(), module.__dict__)  # pylint: disable=W0122
+            code = compile(
+                source=file.read(),
+                filename=f"{self.module_name}:{module.__file__}",
+                mode="exec",
+                dont_inherit=True,
+            )
+            exec(code, module.__dict__)  # pylint: disable=W0122
 
     def get_data(self, path):
         """ Read data resource """
+        if os.sep != posixpath.sep:
+            path = path.replace(os.sep, posixpath.sep)
+        #
         try:
             with self.storage.open(path, "r") as file:
                 data = file.read()
             return data
         except BaseException as exc:
-            raise OSError("Resource not found") from exc
+            raise FileNotFoundError(f"Resource not found: {path}") from exc
+
+    def has_directory(self, path):
+        """ Check if directory is present in module """
+        if os.sep != posixpath.sep:
+            path = path.replace(os.sep, posixpath.sep)
+        #
+        if not path.endswith(posixpath.sep):
+            path = f"{path}{posixpath.sep}"
+        #
+        return path in self.storage_files
+
+    def get_resource_reader(self, fullname):
+        """ Get ResourceReader """
+        name_components = fullname.split(".")
+        return DataModuleResourceReader(
+            self, posixpath.sep.join(name_components[len(self.module_name_components):])
+        )
 
 
 class DataModuleProvider(pkg_resources.NullProvider):  # pylint: disable=W0223
     """ Allows to load resources from ZIP in-memory data """
 
-    def __init__(self, module):
-        pkg_resources.NullProvider.__init__(self, module)
-        self.module_name = getattr(module, "__name__", "")
-
     def _has(self, path):
-        return path in self.loader.storage_files
+        if os.sep != posixpath.sep:
+            path = path.replace(os.sep, posixpath.sep)
+        #
+        return \
+            not path or path in self.loader.storage_files or f"{path}/" in self.loader.storage_files
+
+    def _isdir(self, path):
+        if os.sep != posixpath.sep:
+            path = path.replace(os.sep, posixpath.sep)
+        #
+        if path in self.loader.storage_files:
+            return path.endswith(posixpath.sep)
+        if not path or f"{path}/" in self.loader.storage_files:
+            return True
+        #
+        return False
+
+    def _listdir(self, path):
+        if os.sep != posixpath.sep:
+            path = path.replace(os.sep, posixpath.sep)
+        #
+        if not self._has(path):
+            raise FileNotFoundError(f"Path not found: {path}")
+        if not self._isdir(path):
+            raise NotADirectoryError(f"Not a directory: {path}")
+        #
+        components = len(path.split(posixpath.sep)) if path else 0
+        #
+        files = [
+            item.split(posixpath.sep)[-1] for item in self.loader.storage_files
+            if item.split(posixpath.sep)[-1] and
+            len(item.split(posixpath.sep)) == components + 1
+        ]
+        dirs = [
+            item.split(posixpath.sep)[-2] for item in self.loader.storage_files
+            if not item.split(posixpath.sep)[-1] and
+            len(item.split(posixpath.sep)) == components + 2
+        ]
+        #
+        return files + dirs
+
+
+class DataModuleResourceReader(importlib.abc.ResourceReader):
+    """ Allows to read resources from ZIP in-memory data """
+
+    def __init__(self, loader, path):
+        self.loader = loader
+        self.path = path
+
+    def open_resource(self, resource):
+        """ Implementation of open_resource """
+        if os.sep != posixpath.sep:
+            resource = resource.replace(os.sep, posixpath.sep)
+        #
+        try:
+            return self.loader.storage.open(resource, "r")
+        except BaseException as exc:
+            raise FileNotFoundError(f"Resource not found: {resource}") from exc
+
+    def resource_path(self, resource):
+        """ Implementation of resource_path """
+        if os.sep != posixpath.sep:
+            resource = resource.replace(os.sep, posixpath.sep)
+        #
+        raise FileNotFoundError(f"Path to resource not found: {resource}")
+
+    def is_resource(self, name):
+        """ Implementation of is_resource """
+        if os.sep != posixpath.sep:
+            name = name.replace(os.sep, posixpath.sep)
+        #
+        if name in self.loader.storage_files:
+            return not name.endswith(posixpath.sep)
+        #
+        return False
+
+    def contents(self):
+        """ Implementation of contents """
+        path = self.path
+        #
+        if os.sep != posixpath.sep:
+            path = path.replace(os.sep, posixpath.sep)
+        #
+        components = len(path.split(posixpath.sep)) if path else 0
+        #
+        files = [
+            item.split(posixpath.sep)[-1] for item in self.loader.storage_files
+            if item.split(posixpath.sep)[-1] and
+            len(item.split(posixpath.sep)) == components + 1
+        ]
+        dirs = [
+            item.split(posixpath.sep)[-2] for item in self.loader.storage_files
+            if not item.split(posixpath.sep)[-1] and
+            len(item.split(posixpath.sep)) == components + 2
+        ]
+        #
+        return files + dirs
