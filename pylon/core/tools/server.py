6f07ba3d08db99eef9a04d26c290f7033fc0ca3d
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2023-10-15 21:14:54 +0300

    Year update

--- pylon/core/tools/server.py
+++ pylon/core/tools/server.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 # pylint: disable=I0011
 
-#   Copyright 2021 getcarrier.io
+#   Copyright 2023 getcarrier.io
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
