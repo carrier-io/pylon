2cdcaed52be412773f569a08df02ae648876de2f
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-02-05 14:29:20 +0200

    Exposure: PoC

--- pylon/core/tools/exposure.py
+++ pylon/core/tools/exposure.py
@@ -22,7 +22,11 @@
 
 import io
 
+import flask  # pylint: disable=E0401
+import arbiter  # pylint: disable=E0401
+
 from pylon.core.tools import log
+from pylon.core.tools.context import Context
 
 from tools import context  # pylint: disable=E0401,C0411
 
@@ -30,34 +34,176 @@ from tools import context  # pylint: disable=E0401,C0411
 def expose():
     """ Expose this pylon over pylon network """
     log.info("Exposing pylon")
-    rpc_id = f"pylon_{context.id}"
     #
-    context.rpc_manager.register_function(
-        ping, name=f"{rpc_id}_ping"
+    context.exposure = Context()
+    context.exposure.id = f"pylon_{context.id}"
+    context.exposure.config = context.settings.get("exposure", {})
+    context.exposure.event_node = None
+    context.exposure.rpc_node = None
+    #
+    # Config
+    #
+    config = context.exposure.config
+    if not config or "event_node" not in config:
+        return
+    #
+    # EventNode
+    #
+    context.exposure.event_node = arbiter.make_event_node(
+        config=config.get("event_node"),
     )
+    context.exposure.event_node.start()
     #
-    context.rpc_manager.register_function(
-        wsgi_call, name=f"{rpc_id}_wsgi_call"
+    if config.get("handle", False):
+        context.exposure.event_node.subscribe(
+            "pylon_exposed", on_pylon_exposed
+        )
+        #
+        context.exposure.event_node.subscribe(
+            "pylon_unexposed", on_pylon_unexposed
+        )
+    #
+    # RpcNode
+    #
+    context.exposure.rpc_node = arbiter.RpcNode(
+        context.exposure.event_node,
+        id_prefix=f"exposure_rpc_{context.id}_",
     )
+    context.exposure.rpc_node.start()
     #
-    # Need: use separate RpcNode (use arbiter.make_event_node)
-    # Next: SIO, as it needs special handling
-    # Also: streaming, caching and so on
-    # And: announce to other pylons... and handle announces
+    if config.get("expose", False):
+        context.exposure.rpc_node.register(
+            ping, name=f"{context.exposure.id}_ping"
+        )
+        #
+        context.exposure.rpc_node.register(
+            wsgi_call, name=f"{context.exposure.id}_wsgi_call"
+        )
+        #
+        context.exposure.event_node.emit(
+            "pylon_exposed",
+            {
+                "exposure_id": context.exposure.id,
+                "url_prefix": context.url_prefix,
+            },
+        )
+    #
+    # Next: periodic announce to other pylons... and handle announces
+    # And: SIO, as it needs special handling
+    # Later: streaming, caching and so on
 
 
 def unexpose():
     """ Unexpose this pylon over pylon network """
     log.info("Unexposing pylon")
-    rpc_id = f"pylon_{context.id}"
     #
-    context.rpc_manager.unregister_function(
-        wsgi_call, name=f"{rpc_id}_wsgi_call"
+    if context.exposure.event_node is None:
+        return
+    #
+    if context.exposure.config.get("expose", False):
+        context.exposure.event_node.emit(
+            "pylon_unexposed",
+            {
+                "exposure_id": context.exposure.id,
+            },
+        )
+        #
+        context.exposure.rpc_node.unregister(
+            wsgi_call, name=f"{context.exposure.id}_wsgi_call"
+        )
+        #
+        context.exposure.rpc_node.unregister(
+            ping, name=f"{context.exposure.id}_ping"
+        )
+    #
+    context.exposure.rpc_node.stop()
+    #
+    if context.exposure.config.get("handle", False):
+        context.exposure.event_node.unsubscribe(
+            "pylon_unexposed", on_pylon_unexposed
+        )
+        #
+        context.exposure.event_node.unsubscribe(
+            "pylon_exposed", on_pylon_exposed
+        )
+    #
+    context.exposure.event_node.stop()
+
+
+def on_pylon_exposed(event_name, event_payload):
+    """ Event callback """
+    _ = event_name
+    exposure_id = event_payload.get("exposure_id")
+    #
+    if exposure_id == context.exposure.id:
+        return
+    #
+    base_url = f'{event_payload.get("url_prefix")}/'
+    view_func = make_view_func(exposure_id)
+    #
+    context.app.add_url_rule(
+        base_url,
+        endpoint=f"pylon_exposure_{exposure_id}",
+        view_func=view_func,
+        defaults={"sub_path": ""},
     )
     #
-    context.rpc_manager.unregister_function(
-        ping, name=f"{rpc_id}_ping"
+    context.app.add_url_rule(
+        f'{base_url}/<path:sub_path>',
+        endpoint=f"pylon_exposure_{exposure_id}_sub_path",
+        view_func=view_func,
     )
+
+
+def on_pylon_unexposed(event_name, event_payload):
+    """ Event callback """
+    _ = event_name, event_payload
+
+
+def make_view_func(exposure_id):
+    """ Make exposure handler """
+    #
+    def on_request(sub_path):
+        """ Exposure handler """
+        _ = sub_path
+        #
+        log.info("Target: %s", exposure_id)
+        #
+        wsgi_environ = flask.request.environ
+        log.info("WSGI env [input]: %s", wsgi_environ)
+        #
+        call_environ = prepare_rpc_environ(wsgi_environ)
+        log.info("WSGI env [prepared]: %s", call_environ)
+        #
+        wsgi_result = context.exposure.rpc_node.call(
+            f"{exposure_id}_wsgi_call", call_environ,
+        )
+        #
+        view_rv = (
+            wsgi_result["body"],
+            wsgi_result["status"],
+            wsgi_result["headers"],
+        )
+        #
+        return flask.make_response(view_rv)
+    #
+    return on_request
+
+
+def prepare_rpc_environ(wsgi_environ):
+    """ Prepare environ for wsgi_call """
+    result = dict(wsgi_environ)
+    #
+    drop_keys = [
+        "wsgi.input",
+        "wsgi.errors",
+        "werkzeug.socket",
+    ]
+    #
+    for key in drop_keys:
+        result.pop(key, None)
+    #
+    return result
 
 
 def ping():
