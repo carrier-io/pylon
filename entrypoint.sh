035e847967195098e4518a7947c068b2c4fa7e2b
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-06-17 15:49:17 +0300

    System: fix formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 : "${PYLON_INIT:=dumb-init}"
 
 if [ "$PYLON_INIT" = "pylon" ]; then
-   exec python -m pylon.main
+  exec python -m pylon.main
 elif [ "$PYLON_INIT" = "dumb-init" ]; then
-   exec dumb-init python -m pylon.main
+  exec dumb-init python -m pylon.main
 else
   echo "Unknown init: $PYLON_INIT"
   exit 1
